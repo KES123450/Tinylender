f48ddef0411e2eff7bcec3b39323caa74111aed3
김은서 <94689360+KES123450@users.noreply.github.com>
2024-04-23 22:08:55 +0900

    [Feat] InputEventSystem 구현 중

--- src/InputEventSystem.h
+++ src/InputEventSystem.h
@@ -8,14 +8,27 @@ enum eInputState{
     NotChanged
 }
 
+
 class InputEventSystem{
     public:
-        glm::vec2 position;
+        glm::vec2 inputPosition;
         eInputState inputState;
 
+        // 마우스 움질일 때마다 감지해서 inputPosition 변경
         void inputPos(GLFWwindow* window, double xpos, double ypos);
+        //현재 있는 canvas에 접근하여 지금 인풋 위치 범위에 있는 UI의 callback함수 호출
         void inputButton(GLFWwindow* window, int button, int action, int mods)
 
+        glm::vec2 getMousePos() const {return inputPosition;}
 
-
+    private:
+        vec<Ipushed>
+        vec<IClicked>
 }
+
+InputEventSystem::inputPos(GLFWwindow* window, double xpos, double ypos){
+    inputPosition.x = xpos;
+    inputPosition.y= ypos;
+}
+
+
