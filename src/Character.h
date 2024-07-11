37c8d3565733980365250fbc0b2ae31601e79cbe
김은서 <94689360+KES123450@users.noreply.github.com>
2024-07-11 20:06:31 +0900

    [Chore] freetype 라이브러리 추가

---
+++ src/Character.h
@@ -0,0 +1,11 @@
+#include <glm/glm.hpp>
+
+struct Character
+{
+    unsigned int TextureID;  // ID handle of the glyph texture
+    glm::ivec2   Size;       // Size of glyph
+    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+    unsigned int Advance; 
+};
+
+std::map<char, Character> Characters;
